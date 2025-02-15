03bb8501defaf24deea17cbb143dcea7ff59c5b4
Chaul <cagilpekoz@gmail.com>
2025-02-15 23:13:44 +0100

    init
    init

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  eventStartMinute: 52, // The minute of the hour when the event starts (0-59)
+  eventStartSecond: 25   // The second of that minute when the event starts (0-59)
+};
