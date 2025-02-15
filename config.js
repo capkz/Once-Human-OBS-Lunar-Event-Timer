4cc1f40855692d8003809cd65885256d4972f355
capkz <cagilpekoz@gmail.com>
2025-02-15 23:48:38 +0100

    Refactor for Railway

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-const CONFIG = {
-  eventStartMinute: 52, // The minute of the hour when the event starts (0-59)
-  eventStartSecond: 25   // The second of that minute when the event starts (0-59)
-};
