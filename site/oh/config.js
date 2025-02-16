f1e648018a1435ad05c277c9624c22bdd4ccb942
capkz <cagilpekoz@gmail.com>
2025-02-16 13:47:24 +0100

    Added Admin Panel for Lunar

--- site/oh/config.js
+++ site/oh/config.js
@@ -1,5 +1,24 @@
 // config.js
-const CONFIG = {
-  eventStartMinute: 52, // The minute of the hour when the event starts (0-59)
-  eventStartSecond: 25   // The second of that minute when the event starts (0-59)
+
+// Set your default values
+var CONFIG = {
+  eventStartMinute: 52,
+  eventStartSecond: 25
 };
+
+// Check for URL query parameters (e.g., ?minute=30&second=10)
+(function() {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has('minute')) {
+    const minute = parseInt(params.get('minute'), 10);
+    if (!isNaN(minute)) {
+      CONFIG.eventStartMinute = minute;
+    }
+  }
+  if (params.has('second')) {
+    const second = parseInt(params.get('second'), 10);
+    if (!isNaN(second)) {
+      CONFIG.eventStartSecond = second;
+    }
+  }
+})();
