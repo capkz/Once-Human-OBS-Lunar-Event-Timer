5d791799f799e7b49464afcce94a305680e81289
capkz <cagilpekoz@gmail.com>
2025-02-16 14:05:36 +0100

    Added Admin Panel for Lunar

--- site/oh/config.js
+++ site/oh/config.js
@@ -1,24 +1,30 @@
 // config.js
-
-// Set your default values
 var CONFIG = {
   eventStartMinute: 52,
   eventStartSecond: 25
 };
 
-// Check for URL query parameters (e.g., ?minute=30&second=10)
 (function() {
   const params = new URLSearchParams(window.location.search);
+  // If query parameters exist, override the defaults
   if (params.has('minute')) {
-    const minute = parseInt(params.get('minute'), 10);
-    if (!isNaN(minute)) {
-      CONFIG.eventStartMinute = minute;
-    }
+    CONFIG.eventStartMinute = parseInt(params.get('minute'), 10) || CONFIG.eventStartMinute;
   }
   if (params.has('second')) {
-    const second = parseInt(params.get('second'), 10);
-    if (!isNaN(second)) {
-      CONFIG.eventStartSecond = second;
-    }
+    CONFIG.eventStartSecond = parseInt(params.get('second'), 10) || CONFIG.eventStartSecond;
+  }
+  // If one of the parameters is missing, update the URL with the default values.
+  let updated = false;
+  if (!params.has('minute')) {
+    params.set('minute', CONFIG.eventStartMinute);
+    updated = true;
+  }
+  if (!params.has('second')) {
+    params.set('second', CONFIG.eventStartSecond);
+    updated = true;
+  }
+  if (updated) {
+    const newUrl = window.location.pathname + '?' + params.toString();
+    window.history.replaceState({}, '', newUrl);
   }
 })();
